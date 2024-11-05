369082fb3b6af47331973d9ca64d76deae560c6a
jakefish18 <jakefish.work@gmail.com>
2024-11-05 12:29:55 +0300

    Add requirements.txt and .env.example

--- code/config.py
+++ code/config.py
@@ -10,15 +10,18 @@ Database configs:
     DATABASE_USERNAME : database owner username for connection
     DATABASE_PASSWORD : database password
 """
+import dotenv
+dotenv.load_dotenv()
+import os
 
 # Bot configs.
-BOT_TOKEN = ""
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Database configs.
-DATABASE_HOST = "127.0.0.1" 
-DATABASE_NAME = ""
-DATABASE_USERNAME = ""
-DATABASE_PASSWORD = ""
+DATABASE_HOST = os.getenv("DATABASE_HOST") 
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 
 #Server configs.
-SERVER_TIME_ZONE = "UTC+00:00"
+SERVER_TIME_ZONE = os.getenv("SERVER_TIME_ZONE")
